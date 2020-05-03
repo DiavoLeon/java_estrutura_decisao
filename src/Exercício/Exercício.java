3eb9e5b3a3bf7cd647a687f9ad89cd5a549cbeee
unknown <labreu690@gmail.com>
2020-05-03 19:49:54 -0300

    Exercício 3

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -1,7 +1,23 @@
 package Exercício;
 
+import java.util.Scanner;
+
 public class Exercício {
 	public static void main(String[] agrs) {
+		
+		Scanner leitor = new Scanner (System.in);
+		int x;
+		
+		System.out.println("Digite um Número: ");
+		x = leitor.nextInt();
+		
+		if (x > 0) {
+			System.out.println("Positivo");
+		}
+		else if (x < 0) {
+			System.out.println("Negativo");
+		}
+			
 		
 	}
 	
