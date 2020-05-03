272e5a43a892031cac93a0b8b3207e231e72972d
unknown <labreu690@gmail.com>
2020-05-03 19:31:23 -0300

    Exercício 2

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -1,7 +1,25 @@
 package Exercício;
+import java.util.Scanner;
 
 public class Exercício {
 	public static void main(String[] agrs) {
+		
+		int x, y, soma;
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		System.out.println("Digite um Número: ");
+		x = leitor.nextInt();
+		
+		System.out.println("Digite um Segundo Número: ");
+		y = leitor.nextInt();
+		
+		soma = x + y;
+		
+		if (soma>10)
+		{
+			System.out.println(soma);
+		}
 		
 	}
 	
