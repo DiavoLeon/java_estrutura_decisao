c22edd22d7df4f585b0f7ab059641c927a084532
unknown <labreu690@gmail.com>
2020-05-03 19:34:19 -0300

    Exercício 1

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -1,7 +1,21 @@
 package Exercício;
+import java.util.Scanner;
 
 public class Exercício {
 	public static void main(String[] agrs) {
+		
+		int x;
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		System.out.println("Digite um Número: ");
+		x = leitor.nextInt();
+
+		if (x > 20)
+		{
+			System.out.println(x);
+		}
+
 		
 	}
 	
