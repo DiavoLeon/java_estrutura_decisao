b3d8d4fdef03ce8e796205db8ee86d92f3bde104
unknown <labreu690@gmail.com>
2020-05-04 15:37:59 -0300

    Exercício 4

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -1,8 +1,28 @@
 package Exercício;
 
+import java.util.Scanner;
+
 public class Exercício {
 	public static void main(String[] agrs) {
+		int x, y;
 		
+		Scanner leitor = new Scanner (System.in);
+		
+		System.out.println("Digite um Número: ");
+		x = leitor.nextInt();
+		
+		System.out.println("Digite um Segundo Número: ");
+		y = leitor.nextInt();
+		
+		if (x>y) {
+			System.out.println(x);
+			System.out.println(y);
+		}
+		else {
+			System.out.println(y);
+			System.out.println(x);
+		}
+			
 	}
 	
 }
