eb4d217bc02a9a8723a4d11b81d5394fa4275a67
unknown <labreu690@gmail.com>
2020-05-04 20:23:25 -0300

    Exercício 6

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -7,6 +7,30 @@ public class Exercício
 	public static void main(String[] agrs) 
 	{
 		Scanner leitor = new Scanner (System.in);
+		float x, y, z, total, soma;
+		x = 2;
+		y = 3;
+		z = 5;
+		soma = x + y + z;
+		
+		System.out.println("Digite a primeira nota: ");
+		x = leitor.nextFloat() * x;
+		
+		System.out.println("Digite a segunda nota: ");
+		y = leitor.nextFloat() * y;
+		
+		System.out.println("Digite a terceira nota: ");
+		z = leitor.nextFloat() * z;
+		
+		total = x + y + z;
+		
+		if(total / soma >= 7) {
+			System.out.println("APROVADO");
+		}
+		
+		else {
+			System.out.println("REPROVADO");
+		}
 	}
 	
 }
