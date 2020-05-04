4dff7ca9dee58d79ad2e82511d5cc05f92963110
unknown <labreu690@gmail.com>
2020-05-04 20:39:23 -0300

    Exercício 8

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -7,6 +7,17 @@ public class Exercício
 	public static void main(String[] agrs) 
 	{
 		Scanner leitor = new Scanner (System.in);
+		String x; 
+		
+		System.out.println("Digite A Senha: ");
+		x = leitor.next();
+		
+		if(x.compareTo("AEDB") == 0) {
+			System.out.println("Acesso Permitido");
+		}
+		else {
+			System.out.println("Você não tem acesso ao sistema");
+		}
 	}
 	
 }
