7a8b85d184f1c626d89dba6af6917c41d13cd7c9
unknown <labreu690@gmail.com>
2020-05-04 19:57:56 -0300

    Exercício 7

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -1,4 +1,5 @@
 package Exercício;
+
 import java.util.Scanner;
 
 public class Exercício 
@@ -6,7 +7,30 @@ public class Exercício
 	
 	public static void main(String[] agrs) 
 	{
+		int x, y;
+		String a, b;
+		
 		Scanner leitor = new Scanner (System.in);
+		
+		System.out.println("Bem Vindo Pessoa (A). Por favor digite seu nome: ");
+		a = leitor.next();
+		
+		System.out.println("Digite sua Idade: ");
+		x = leitor.nextInt();
+		
+		System.out.println("Bem Vindo Pessoa (B). Por favor digite seu nome: ");
+		b = leitor.next();
+		
+		System.out.println("Digite sua Idade: ");
+		y = leitor.nextInt();
+		
+		if(x>y) {
+			System.out.println("Nome : "+ a + " \nIdade: " + x);
+			
+		}
+		else {
+			System.out.println("Nome : "+ b + " \nIdade: " + y);
+		}
 	}
 	
 }
