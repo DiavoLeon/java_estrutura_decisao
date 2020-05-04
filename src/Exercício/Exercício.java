70f6c618e04bb9a7f3efd6fa0f370cf14a3c404b
unknown <labreu690@gmail.com>
2020-05-04 17:04:09 -0300

    Exercício 5

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -1,8 +1,26 @@
 package Exercício;
 
+import java.util.Scanner;
+
 public class Exercício {
-	public static void main(String[] agrs) {
+	public static void main(String[] agrs) 
+	{
+		int x;
 		
+		Scanner leitor = new Scanner (System.in);
+		
+		System.out.println("Digite um Número: ");
+		x = leitor.nextInt();
+		
+		if(x % 2 == 0 ) 
+		{
+		System.out.println("Esse Número é Par");
+		}
+		else  
+		{
+		System.out.println("Esse Número é Impar");	
+		}
+	
 	}
 	
 }
