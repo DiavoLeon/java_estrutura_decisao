813ea6696035d8f9cea6dae84c29fc716695e25d
unknown <labreu690@gmail.com>
2020-05-04 21:19:50 -0300

    Exercício 10

--- src/Exercício/Exercício.java
+++ src/Exercício/Exercício.java
@@ -1,5 +1,8 @@
 package Exercício;
+
 import java.util.Scanner;
+
+import java.text.DecimalFormat;
 
 public class Exercício 
 {
@@ -7,6 +10,28 @@ public class Exercício
 	public static void main(String[] agrs) 
 	{
 		Scanner leitor = new Scanner (System.in);
+		int x; 
+		double y, z;
+		
+		DecimalFormat df = new DecimalFormat();
+        df.applyPattern("KG #,##0.00");
+        
+		
+		System.out.println("Digite 1 para Masculino, 2 para Feminino");
+		
+		x = leitor.nextInt();
+		
+		System.out.println("Digite sua Altura");
+		y = leitor.nextDouble();
+		
+		if(x == 1) {
+			z = (72.7 * y - 58);
+			System.out.println("Seu Peso Ideal é: " + df.format(z));
+		}
+		else {
+			z = (62.1 * y - 44.7);
+			System.out.println("Seu Peso Ideal é: " + df.format(z));
+		}
 	}
 	
 }
